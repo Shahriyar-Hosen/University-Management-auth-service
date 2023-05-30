a145b3b806db86b684e9c458ca74c13e2b008f4f
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-05-30 10:13:27 +0600

    [lint-staged] setup done

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req: Request, res: any) => {
+app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
 
