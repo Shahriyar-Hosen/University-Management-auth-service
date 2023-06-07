992dafa33a57b09ea0b7cae6df956b4ac903afec
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-06-07 19:42:51 +0600

    User Interface and model Create done

---
+++ src/app/modules/academicSemester/as.model.ts
@@ -0,0 +1,39 @@
+import { Schema, model } from "mongoose";
+import { AcademicSemesterModel, IAcademicSemester } from "./as.interface";
+
+const academicSemesterSchema = new Schema<IAcademicSemester>(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    code: {
+      type: String,
+      required: true,
+    },
+    year: {
+      type: Number,
+      required: true,
+    },
+    startMonth: {
+      type: String,
+      required: true,
+    },
+
+    endMonth: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const AcademicSemester = model<IAcademicSemester, AcademicSemesterModel>(
+  "AcademicSemester",
+  academicSemesterSchema
+);
+
+export default AcademicSemester;
