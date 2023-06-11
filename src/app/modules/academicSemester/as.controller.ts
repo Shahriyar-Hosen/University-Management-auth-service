3eb2d246b540e30994b9407b7f9c0710485ddb39
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-06-11 15:54:19 +0600

    catchAsync solved type error

--- src/app/modules/academicSemester/as.controller.ts
+++ src/app/modules/academicSemester/as.controller.ts
@@ -1,21 +1,21 @@
-import { RequestHandler } from "express";
+import { NextFunction, Request, Response } from "express";
+import catchAsync from "../../../shared/catchAsync";
 import { AcademicSemesterService } from "./as.service";
 
-const createSemester: RequestHandler = async (req, res, next) => {
-  try {
+const createSemester = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
     const { ...data } = req.body;
 
     const result = await AcademicSemesterService.crateSemester(data);
 
+    next();
     res.status(200).json({
       success: true,
       message: "Academic semester is created successfully!",
       data: result,
     });
-  } catch (err) {
-    next(err);
   }
-};
+);
 
 export const AcademicSemesterController = {
   createSemester,
--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,21 +1,21 @@
-import { RequestHandler } from "express";
+import { NextFunction, Request, Response } from "express";
+import catchAsync from "../../../shared/catchAsync";
 import { UserService } from "./user.service";
 
-const createUser: RequestHandler = async (req, res, next) => {
-  try {
+const createUser = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
     const { user } = req.body;
 
     const result = await UserService.createUser(user);
 
+    next();
     res.status(200).json({
       success: true,
       message: "User created successfully!",
       data: result,
     });
-  } catch (err) {
-    next(err);
   }
-};
+);
 
 export const UserController = {
   createUser,
