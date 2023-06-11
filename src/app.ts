57eb63e62b329ec7068a53d940d840eebd49cf6a
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-06-11 15:38:05 +0600

    Optimize Application Routes

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,7 @@ import cors from "cors";
 import express, { Application } from "express";
 import { globalErrorHandler } from "./app/middlewares";
 
-import { AcademicSemesterRoutes } from "./app/modules/academicSemester/as.route";
-import { UserRoutes } from "./app/modules/user/user.route";
+import routes from "./app/routes";
 
 const app: Application = express();
 
@@ -15,8 +14,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Application Route
-app.use("/api/v1/users/", UserRoutes);
-app.use("/api/v1/academic-semester/", AcademicSemesterRoutes);
+app.use("/api/v1/", routes);
+// app.use("/api/v1/academic-semester/", AcademicSemesterRoutes);
 
 //Testing
 // app.get("/", async (req: Request, res: Response, next: NextFunction) => {
