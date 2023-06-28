742675ef1da19788e98850e7e5f374a297434e2e
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-06-28 11:10:37 +0600

    testing

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,49 @@
+import { Server } from "http";
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./config/index";
+import { errLogger, logger } from "./shared/logger";
+
+process.on("uncaughtException", error => {
+  errLogger.error(error);
+  process.exit(1);
+});
+
+let server: Server;
+
+async function bootstrap() {
+  try {
+    await mongoose.connect(config.database_url as string);
+    logger.info(`Database is Connected Successfully! ✅📦✅`);
+
+    server = app.listen(config.port, () => {
+      logger.info(`Server is app listening on port ${config.port} 🫀✅🫀`);
+    });
+  } catch (err) {
+    errLogger.error("❌❌❌ Database connection failed‼ error:- " + err);
+  }
+
+  process.on("unhandledRejection", error => {
+    if (server) {
+      server.close(() => {
+        errLogger.error(error);
+        process.exit(1);
+      });
+    } else {
+      process.exit(1);
+    }
+  });
+}
+
+bootstrap();
+
+process.on("SIGTERM", () => {
+  logger.info("SIGTERM is received");
+  if (server) {
+    server.close();
+  }
+});
+
 // import { Server } from "http";
 // import mongoose from "mongoose";
 // import app from "./app";
@@ -44,49 +90,3 @@
 //     server.close();
 //   }
 // });
-
-import { Server } from "http";
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config/index";
-import { errLogger, logger } from "./shared/logger";
-
-process.on("uncaughtException", error => {
-  errLogger.error(error);
-  process.exit(1);
-});
-
-let server: Server;
-
-async function bootstrap() {
-  try {
-    await mongoose.connect(config.database_url as string);
-    logger.info(`Database is Connected Successfully! ✅📦✅`);
-
-    server = app.listen(config.port, () => {
-      logger.info(`Server is app listening on port ${config.port} 🫀✅🫀`);
-    });
-  } catch (err) {
-    errLogger.error("❌❌❌ Database connection failed‼ error:- " + err);
-  }
-
-  process.on("unhandledRejection", error => {
-    if (server) {
-      server.close(() => {
-        errLogger.error(error);
-        process.exit(1);
-      });
-    } else {
-      process.exit(1);
-    }
-  });
-}
-
-bootstrap();
-
-process.on("SIGTERM", () => {
-  logger.info("SIGTERM is received");
-  if (server) {
-    server.close();
-  }
-});
