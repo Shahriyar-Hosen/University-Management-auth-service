616e9d8069d3ce6e09f42827cf7eabf67b8e1423
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-07-16 23:14:58 +0600

    clean up

--- src/app.ts
+++ src/app.ts
@@ -42,7 +42,7 @@ app.use((req: Request, res: Response, next: NextFunction) => {
 export default app;
 
 // const as = {
-//   code: "01",
+//   code: "01
 
 const testId = async () => {
   const testId = await generateFacultyId();
