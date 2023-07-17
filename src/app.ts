e5c270f8afadcbb16772ae0b27e0562711b7644e
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-07-17 23:36:45 +0600

    update and clean up

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,7 @@ app.use("/api/v1", routes);
 
 //Testing
 // app.get('/', async (req: Request, res: Response, next: NextFunction) => {
-//   throw new
+//   throw
 
 //global error handler
 app.use(globalErrorHandler);
