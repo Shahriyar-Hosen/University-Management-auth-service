9f6172db053fd1415aa1ef0f613a53760b7c743b
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-07-17 23:37:36 +0600

    update and clean up

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api/v1", routes);
 
 //Testing
-// app.get('/', async (req: Request, res: Response,
+// app.get('/', async (req: Request, res:
 
 //global error handler
 app.use(globalErrorHandler);
