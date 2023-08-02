2d31977f4616efcca7472f505d97eaf0ea5c2f14
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-08-02 21:14:28 +0600

    clean up

--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -99,15 +99,16 @@ const updateStudent = async (
   }
   if (guardian && Object.keys(guardian).length > 0) {
     Object.keys(guardian).forEach(key => {
-      const guardianKey = `guardian.${key}` as keyof Partial<IStudent>; // `guardian.fisrtguardian`
+      const guardianKey = `guardian.${key}` as keyof Partial<IStudent>;
       (updatedStudentData as any)[guardianKey] =
-        guardian[key as keyof typeof guardian]; // updatedStudentData['
+        guardian[key as keyof typeof guardian]; // updatedStudentData['guardian.motherContactNo']=guardian[motherContactNo]
+      // updatedStudentData --> object create --> guardian : { motherContactNo: 0177}
     });
   }
   if (localGuardian && Object.keys(localGuardian).length > 0) {
     Object.keys(localGuardian).forEach(key => {
       const localGuradianKey =
-        `localGuardian.${key}` as keyof Partial<IStudent>; // `localGuardian.fisrtName`
+        `localGuardian.${key}` as keyof Partial<IStudent>;
       (updatedStudentData as any)[localGuradianKey] =
         localGuardian[key as keyof typeof localGuardian];
     });
