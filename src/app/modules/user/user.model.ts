95773ab6010e7db35d952f3277b585ef3cdfb83c
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-08-07 13:23:30 +0600

    update user model

--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -19,6 +19,10 @@ const userSchema = new Schema<IUser>(
       required: true,
       select: 0,
     },
+    needsPasswordChange: {
+      type: Boolean,
+      default: true,
+    },
     student: {
       type: Schema.Types.ObjectId,
       ref: "Student",
