eea1c371917d10ca4f508a023348c3c8cbe3e838
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-08-15 21:11:25 +0600

    update comment

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -21,7 +21,7 @@ const auth =
 
       req.user = verifiedUser; // role , userid
 
-      // role diye guard korar jnno
+      // role base authorized
       if (requiredRoles.length && !requiredRoles.includes(verifiedUser.role)) {
         throw new ApiError(httpStatus.FORBIDDEN, "Forbidden");
       }
